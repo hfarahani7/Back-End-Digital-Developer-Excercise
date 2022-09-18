73c013af49f7c17600b7f9c026e7f5ddce2038e0
hfarahani7 <hfarahani7@gmail.com>
2022-09-18 16:03:01 -0400

    Add index template, logic to fetch events & categories, and logic to map response JSON to template

--- backendDev/eventPaginator/views.py
+++ backendDev/eventPaginator/views.py
@@ -1,25 +1,60 @@
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 import requests
 import json
 
 eventCategories = [{}]
-events = [[{}]]
+events = [{}]
+event_list = [{}]
 categoriesUrl = "https://www.adelphi.edu/wp-json/wp/v2/event_category"
 eventUrl = "https://www.adelphi.edu/wp-json/wp/v2/event"
 
 def index(request):
-    # return HttpResponse("Test")
-    categoryJSON = getCategories()
-    return HttpResponse(categoryJSON)
+    eventCategories = getCategories()
+    events = getEvents()
+
+    for event in events:
+        event_list.append({
+            "link": event.link,
+            "title": event.title,
+            "category": event.category,
+            "start": event.start,
+            "end": event.end,
+            "location": event.location
+        })
+
+    context = {'event_list': event_list}
+    template = loader.get_template('eventPaginator/index.html')
+    return HttpResponse(template.render(context, request))
     
 
 def getCategories():
-    try:
-        r = requests.get(categoriesUrl)
-    except:
-        return("502 please try again later")
-    #for category in r.json() do something
-    return(r.json())
+    # #returns list of raw json from WP event categories endpoint
+    currPage = 1
+    r = requests.get(eventUrl + "?page=" + currPage)
+    pageCount = r.header()["X-WP-TotalPages"]
+    eventCategoryJSON = [{}]
+
+    while(currPage <= pageCount):
+        currPage += 1
+        eventCategoryJSON.append(r.json())
+        r = requests.get(categoriesUrl + "?page=" + currPage)
+
+    return(eventCategoryJSON)
 
 def getEvents():
+    #returns list of raw json from WP events endpoint
+    currPage = 1
+    r = requests.get(eventUrl + "?page=" + currPage)
+    pageCount = r.header()["X-WP-TotalPages"]
+    eventJSON = [{}]
+    
+    while(currPage <= pageCount):
+        currPage += 1
+        eventJSON.append(r.json())
+        r = requests.get(eventUrl + "?page=" + currPage)
+
+    return(eventJSON)
